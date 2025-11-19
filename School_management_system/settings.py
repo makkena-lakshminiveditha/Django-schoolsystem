8c05765bd3d46b372e917e14181a349c6d6e8612
makkena-lakshminiveditha <makkenalakshminiveditha@gmail.com>
2025-11-19 15:35:54 +0530

    Added requirements and Procfile for render deployment

--- School_management_system/settings.py
+++ School_management_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--l(cg%pck#6n4s$ib+t3a(5psm8#+&ky0u788=4ys4r_iy6#b$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
