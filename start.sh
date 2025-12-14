4a7cfa3930a2d92cca229e82d188262587fa413f
makkena-lakshminiveditha <makkenalakshminiveditha@gmail.com>
2025-12-14 12:47:28 +0530

    Add start script for Render

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn School_management_system.wsgi:application --biond 0.0.0.0 $PORT
+gunicorn School_management_system.wsgi:application --bind 0.0.0.0:10000
